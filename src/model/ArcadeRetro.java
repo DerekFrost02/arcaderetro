094514252c446c346e4e343b0c34420e78620b87
Diego Taioli <de.taioli@itsprodigi.it>
2025-12-12 14:22:53 +0100

    constructor and Getters and Setters

--- src/model/ArcadeRetro.java
+++ src/model/ArcadeRetro.java
@@ -2,5 +2,41 @@ package model;
 
 public class ArcadeRetro 
 {
+	private String nome;
+	private int anno;
+	private int difficolta;
+	
+	public ArcadeRetro(String nome, int anno, int difficolta) {
+		super();
+		this.nome = nome;
+		this.anno = anno;
+		this.difficolta = difficolta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAnno() {
+		return anno;
+	}
+
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+
+	public int getDifficolta() {
+		return difficolta;
+	}
+
+	public void setDifficolta(int difficolta) {
+		this.difficolta = difficolta;
+	}
+	
+	
 
 }
